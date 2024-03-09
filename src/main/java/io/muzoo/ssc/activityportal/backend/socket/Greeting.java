0523bafec0c3ed947ac14d78e151187a9bf2eb07
JornTumrongwit <jorn1013@gmail.com>
2024-03-09 15:30:19 +0700

    Feat: socket demo

---
+++ src/main/java/io/muzoo/ssc/activityportal/backend/socket/Greeting.java
@@ -0,0 +1,18 @@
+package io.muzoo.ssc.activityportal.backend.socket;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
