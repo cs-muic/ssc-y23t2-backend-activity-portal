32dd4266e87355622e9504e81b427ae51fd5c483
JornTumrongwit <jorn1013@gmail.com>
2024-03-16 20:45:47 +0700

    Deploy: added the scripts to help with the deploying of the sit

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 127.0.0.1:13306:3306 --detach --name ssc-webapp-mariaDB -e MARIADB_ROOT_PASSWORD=GoodPass --restart=always mariadb:10
+docker run -p 127.0.0.1:13306:3306 -p 172.17.0.1:3306:3306 --detach --name ssc-webapp-mariaDB -e MARIADB_ROOT_PASSWORD=GoodPass --restart=always mariadb:10
