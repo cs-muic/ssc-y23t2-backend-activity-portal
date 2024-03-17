3d774296cf9400ca0e31f154690bb38549c6b9cc
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-17 19:25:44 +0700

    feat: Created a many to one relationship between group and activity, create activity is now joined with group system. Group can now see a list of activities they have

--- src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
@@ -5,7 +5,10 @@ import io.muzoo.ssc.activityportal.backend.group.Group;
 import io.muzoo.ssc.activityportal.backend.group.GroupRepository;
 import io.muzoo.ssc.activityportal.backend.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActivityController {
