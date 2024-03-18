004e9a3934d33178022b2efb685c1a6a98a25c66
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-18 15:42:40 +0700

    feat: user can edit the activity

--- src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
@@ -5,10 +5,7 @@ import io.muzoo.ssc.activityportal.backend.group.Group;
 import io.muzoo.ssc.activityportal.backend.group.GroupRepository;
 import io.muzoo.ssc.activityportal.backend.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ActivityController {
@@ -36,6 +33,19 @@ public class ActivityController {
     }
 
 
+//    @GetMapping("api/{activityId}/activity")
+//    public ActivityDTO getActivity(@PathVariable long activityId) {
+//return activityRepository.findById(activityId).map(activity -> {
+//            return ActivityDTO.builder()
+//                    .activity(activity)
+//                    .success(true)
+//                    .message("Activity found")
+//                    .build();
+//        }).orElse(ActivityDTO.builder()
+//                .success(false)
+//                .message("Activity not found")
+//                .build());
+
     /**
      * @param activity Activity object to be created
      * @return SimpleResponseDTO with success/fail and message
@@ -49,27 +59,8 @@ public class ActivityController {
         }).orElse(SimpleResponseDTO.builder().success(false).message("Group not found").build());
     }
 
-    /**
-     * Edit the activity details.
-     * 
-     * @param activityDetail the activity details to be edited.
-     * @param groupId        the group id that the activity should belong to.
-     * @param activityId     the activity id that is going to be edited.
-     * @return SimpleResponseDTO with success/fail and message.
-     */
     @PutMapping("api/{groupId}/activity-edit/{activityId}")
-    public SimpleResponseDTO editActivity(@RequestBody Activity activityDetail, @PathVariable long groupId,
-            @PathVariable long activityId) {
+    public SimpleResponseDTO editActivity(@RequestBody Activity activityDetail, @PathVariable long groupId, @PathVariable long activityId) {
         return activityService.editActivityDetails(activityDetail, groupId, activityId);
-    }
-
-    @PostMapping("api/{groupId}/activity-edit")
-    public SimpleResponseDTO editActivity(@RequestBody Activity activity, @PathVariable long groupId) {
-
-        return groupRepository.findById(groupId).map(group -> {
-            activity.setGroup(group);
-            activityRepository.save(activity);
-            return SimpleResponseDTO.builder().success(true).message("Activity edited").build();
-        }).orElse(SimpleResponseDTO.builder().success(false).message("Group not found").build());
     }
 }
