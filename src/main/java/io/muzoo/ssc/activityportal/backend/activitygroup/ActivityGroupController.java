2590cc476f0b3c497689c7cfd8c01ef1f60ffe93
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-18 15:51:25 +0700

    fix: moved mapToDTO function to class ActivityMapper to reduce duplication

--- src/main/java/io/muzoo/ssc/activityportal/backend/activitygroup/ActivityGroupController.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/activitygroup/ActivityGroupController.java
@@ -48,17 +48,5 @@ public class ActivityGroupController {
         response.setActivities(activities);
         return response;
     }
-
-    private ActivityDTO mapToDTO(Activity activity) {
-        ActivityDTO dto = new ActivityDTO();
-        dto.setId(activity.getId());
-        dto.setName(activity.getName());
-        dto.setStart_time(activity.getStart_time());
-        dto.setEnd_time(activity.getEnd_time());
-        dto.setCleanup_date(activity.getCleanup_date());
-        dto.setAuto_delete_overtime(activity.isAuto_delete_overtime());
-        dto.setDescription(activity.getDescription());
-        return dto;
-    }
 }
 
