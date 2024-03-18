bae3d0dc137a475a9d60c23dc258043517f5b1d7
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-18 15:42:40 +0700

    feat: user can edit the activity

--- src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
@@ -1,6 +1,7 @@
 package io.muzoo.ssc.activityportal.backend.activity;
 
 import io.muzoo.ssc.activityportal.backend.SimpleResponseDTO;
+import io.muzoo.ssc.activityportal.backend.group.Group;
 import io.muzoo.ssc.activityportal.backend.group.GroupRepository;
 import io.muzoo.ssc.activityportal.backend.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,18 @@ public class ActivityController {
     @Autowired
     private GroupRepository groupRepository;
 
+//    @GetMapping("api/{activityId}/activity")
+//    public ActivityDTO getActivity(@PathVariable long activityId) {
+//return activityRepository.findById(activityId).map(activity -> {
+//            return ActivityDTO.builder()
+//                    .activity(activity)
+//                    .success(true)
+//                    .message("Activity found")
+//                    .build();
+//        }).orElse(ActivityDTO.builder()
+//                .success(false)
+//                .message("Activity not found")
+//                .build());
 
     /**
      * @param activity Activity object to be created
