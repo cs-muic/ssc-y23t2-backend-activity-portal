697d44e3666b5301c00eb390ac9ce35e1575ba5a
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-18 15:42:40 +0700

    feat: user can edit the activity

--- src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityService.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityService.java
@@ -6,13 +6,5 @@ import java.util.List;
 
 @Service
 public interface ActivityService {
-    /**
-     * Edit an activity.
-     *
-         * @param activityDetail the activity to be edited.
-     * @param groupId the groupID that the activity belongs to.
-     * @param activityId the activity ID for the activity to be edited.
-     * @return A response DTO informing whether the process was successfull or not.
-     */
     SimpleResponseDTO editActivityDetails(Activity activityDetail, long groupId, long activityId);
 }
