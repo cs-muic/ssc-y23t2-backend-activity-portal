2a893efe599c377bdee1de1e780c57a5bb58ecc1
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-27 10:34:39 +0700

    fix: JSON recursion, added JSON reference to activity and group relationship

--- src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/activity/ActivityController.java
@@ -5,10 +5,7 @@ import io.muzoo.ssc.activityportal.backend.group.Group;
 import io.muzoo.ssc.activityportal.backend.group.GroupRepository;
 import io.muzoo.ssc.activityportal.backend.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ActivityController {
