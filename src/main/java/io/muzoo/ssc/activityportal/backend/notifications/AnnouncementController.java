ade28817905c01dc29ecfa12d8ab14d98f890569
JornTumrongwit <jorn1013@gmail.com>
2024-03-31 13:19:37 +0700

    Feat: messaging functionality

---
+++ src/main/java/io/muzoo/ssc/activityportal/backend/notifications/AnnouncementController.java
@@ -0,0 +1,62 @@
+package io.muzoo.ssc.activityportal.backend.notifications;
+
+import io.muzoo.ssc.activityportal.backend.group.Group;
+import io.muzoo.ssc.activityportal.backend.group.GroupSearchService;
+import io.muzoo.ssc.activityportal.backend.user.User;
+import io.muzoo.ssc.activityportal.backend.whoami.WhoamiService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnnouncementController {
+	@Autowired
+	WhoamiService whoamiService;
+	@Autowired
+	GroupSearchService groupSearchService;
+
+	@MessageMapping("/announcement")
+	@SendTo("/api/topic/announcement")
+	public AnnouncementDTO announce_send(Announcement message) throws Exception {
+		return AnnouncementDTO.builder()
+				.success(true)
+				.message(message.getContent())
+				.group(message.getGroupName())
+				.username(message.getUsername())
+				.build();
+	}
+
+	@PostMapping("/api/send-message")
+	public AnnouncementDTO announce(HttpServletRequest request) throws Exception {
+		try {
+			User u = whoamiService.getCurrentUser();
+			if (u == null) {
+				return AnnouncementDTO.builder()
+						.success(false)
+						.message("User is not logged in")
+						.build();
+			}
+			Announcement message = Announcement.builder()
+					.groupName(request.getParameter("groupName"))
+					.content(request.getParameter("content"))
+					.username(u.getUsername())
+					.groupID(Long.parseLong(request.getParameter("groupID")))
+					.build();
+			announce_send(message);
+			return AnnouncementDTO.builder()
+					.success(true)
+					.message("Message sent")
+					.build();
+		} catch	(Exception e){
+			System.out.println(e);
+			return AnnouncementDTO.builder()
+					.success(false)
+					.message("Error occurred")
+					.build();
+		}
+	}
+}
