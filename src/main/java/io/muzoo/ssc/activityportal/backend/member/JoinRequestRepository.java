d03bc7e65e53019097750defe1e7337dfac6aa5e
Maythas Wangcharoenwong <maythas.wangcharoenwong@gmail.com>
2024-04-04 20:21:27 +0700

    docs: JoinRequestRepository.

--- src/main/java/io/muzoo/ssc/activityportal/backend/member/JoinRequestRepository.java
+++ src/main/java/io/muzoo/ssc/activityportal/backend/member/JoinRequestRepository.java
@@ -7,8 +7,30 @@ import java.util.List;
 
 @Repository
 public interface JoinRequestRepository extends JpaRepository<JoinRequest, Long> {
+    /**
+     * Check if the JoinRequest with respective userID and groupID Exists in the
+     * database.
+     * 
+     * @param userID  (long) : The userID
+     * @param groupID (long) : The groupID
+     * @return true if found, false otherwise
+     */
     boolean existsByUserIDAndGroupID(long userID, long groupID);
+
+    /**
+     * Fetch All JoinRequest from the database with the respective groupID
+     * 
+     * @param groupID (long) : The groupID
+     * @return List<JoinRequest> containing all JoinRequest from the database.
+     */
     List<JoinRequest> findAllByGroupID(long groupID);
 
+    /**
+     * Fetch JoinRequest from the database with the respective userID and groupID
+     * 
+     * @param userID  (long) : The userID
+     * @param groupID (long) : The groupID
+     * @return JoinRequest with the respective userID and groupID
+     */
     JoinRequest findByUserIDAndGroupID(long userID, long groupID);
 }
